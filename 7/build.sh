e1ac13ecab806958194958431448d29011c93505
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2023-12-21 07:35:47 -0500

    Refactor square function. Use math.pow instead of multiplication.

--- 7/build.sh
+++ 7/build.sh
@@ -3,4 +3,14 @@
 IMAGE=singularity-anvio-7.sif
 DEFINITION=Singularity
 
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
 sudo singularity build $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
